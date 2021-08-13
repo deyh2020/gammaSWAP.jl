5c586f914f1792646f6754b407bee9d16e1e2fae
Nathan Foulk <natefoulk4@gmail.com>
2021-08-13 09:07:58 -0400

    Added test with charge noise

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,8 @@ using SpinQubits, Test
 		@test calculateFidelities(4, 0.05, 0.0, 0.0, zeros(3), 10, 1.0)[2][end] ≈ 0.0396766971366405 rtol=1e-8
 	end
 	@testset "Noisy fidelities" begin
-		@test calculateFidelities(4, 0.01, 0.0, 0.0, [0.01, 0.00, 0.00], 500, 3.0)[2][end] ≈ 0.004797717971749105 rtol=1e-1
-		#@test calculateFidelities(4, 0.01, 0.0, 0.0, zeros(3), 10, 1.0)[2][end] ≈ 0.0018048111455146731 rtol=1e-8
-		#@test calculateFidelities(4, 0.05, 0.0, 0.0, zeros(3), 10, 1.0)[2][end] ≈ 0.0396766971366405 rtol=1e-8
+		@test calculateFidelities(4, 0.01, 0.0, 0.0, [0.01, 0.00, 0.00], 5000, 3.0)[2][end] ≈ 0.004784666103416213 rtol=5e-2
+		@test calculateFidelities(4, 0.02, 0.0, 0.0, [0.01, 0.00, 0.00], 5000, 3.0)[2][end] ≈ 0.00951159418717562 rtol=5e-2
 	end
 end
 
