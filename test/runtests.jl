ff39eabe4596cf8686561d2c1d34de0ad5750a04
Nathan Foulk <natefoulk4@gmail.com>
2021-08-13 10:03:33 -0400

    Made tests faster

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using SpinQubits, Test
 
 @testset "Comparison w/ Mathematica tests" begin
 	@testset "Noiseless fidelities" begin
-		@test calculateFidelities(4, 0.00, 0.0, 0.0, zeros(3), 10, 1.0)[2][end] ≈ 0.0000148240466523175 rtol=1e-8
-		@test calculateFidelities(4, 0.01, 0.0, 0.0, zeros(3), 10, 1.0)[2][end] ≈ 0.0018048111455146731 rtol=1e-8
-		@test calculateFidelities(4, 0.05, 0.0, 0.0, zeros(3), 10, 1.0)[2][end] ≈ 0.0396766971366405 rtol=1e-8
+		@test calculateFidelities(4, 0.00, 0.0, 0.0, zeros(3), 10, 3.0)[2][end] ≈ 0.0000148240466523175 rtol=1e-8
+		@test calculateFidelities(4, 0.01, 0.0, 0.0, zeros(3), 10, 3.0)[2][end] ≈ 0.0018048111455146731 rtol=1e-8
+		@test calculateFidelities(4, 0.05, 0.0, 0.0, zeros(3), 10, 3.0)[2][end] ≈ 0.0396766971366405 rtol=1e-8
 	end
 	@testset "Noisy fidelities" begin
 		@test calculateFidelities(4, 0.01, 0.0, 0.0, [0.01, 0.00, 0.00], 5000, 3.0)[2][end] ≈ 0.004784666103416213 rtol=5e-2
