e2e29dcb0d7f77ba56254553294b3bde01e9477a
Nathan Foulk <natefoulk4@gmail.com>
2021-09-27 11:48:08 -0400

    New data varying gamma instead of sigma gamma

--- batch.sh
+++ batch.sh
@@ -7,11 +7,11 @@ for i in `module list`;
 done
 [[ notloaded ]] && module load julia; 
 
-sigJs=( 01 02 03 ); 
-disGams=( 01 05 10 ); 
-sigTs=( 05 10 20 );
-lengs=( 4 6 );
-sings=( false true );
+sigJs=( 01 ); 
+sigYs=( 01 05 10 ); 
+sigTs=( 20 );
+lengs=( 4 );
+sings=( false );
 for sing in "${sings[@]}";
 do
 if [ $sing == true ]
@@ -20,19 +20,19 @@ then
 else
   singstr='up'
 fi
-for ind in {1..10};
+for ind in {2..10};
 do
 for leng in "${lengs[@]}";
 do
 for i in "${sigJs[@]}"; 
-  do for j in "${disGams[@]}"; 
+  do for j in "${sigYs[@]}"; 
     do for k in "${sigTs[@]}"; 
       do 
-	 filename=jdata$ind/mathematica_${leng}_${singstr}_β0.01_γ0.${j}_σJ0.${i}_σγ0.00_στ0.${k}_01000_0.03;
+	 filename=jdata$ind/mathematica_${leng}_${singstr}_β0.01_γ0.10_σJ0.${i}_σγ0.${j}_στ0.${k}_01000_0.03;
 	 #echo $filename
 	 if [ ! -s $filename ]
 	 then
-	   sbatch --time=2:00:00 --mem=2G --ntasks=1 --nodes=1 --mail-user=nfoulk@umd.edu --mail-type=END -J ""$leng""$singstr""$i""_""$j""_""$k"".""$ind"" runner.jl --singlet=$sing --length=$leng --sigmas=0.$i,0.$j,0.$k --nReals=1000 --dirspec=$ind
+	   sbatch --time=2:00:00 --mem=2G --ntasks=1 --nodes=1 --mail-user=nfoulk@umd.edu --mail-type=FAIL -J ""$leng""$singstr""$i""_""$j""_""$k"".""$ind"" runner.jl --singlet=$sing --length=$leng --sigmas=0.$i,0.$j,0.$k --nReals=1000 --dirspec=$ind	
 	 fi 
     done; 
   done;
